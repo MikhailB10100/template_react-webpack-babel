2e27ea2734272ab3c3c43caf35b957764517d6c5
Mikhail Boreysho <mikhailboreysho@gmail.com>
2021-07-01 19:58:56 +0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 import './scss/index.scss'
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="hello">Hello, World!</div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
